36cea5402b37c42ddf3c2f42a12665b4e9fca7bb
minerchow <minerchow@126.com>
2017-11-02 21:38:18 +0800

    引入ts报错

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <hello></hello>
+    <test></test>
   </div>
 </template>
 
@@ -8,11 +9,13 @@
   import Vue from 'vue'
   import Component from 'vue-class-component'
   import Hello from './components/hello.vue';
+  import test from "./components/test.vue";
   import VueResource from "vue-resource";
   Vue.use(VueResource);
   @Component({
     components: {
-      Hello
+      Hello,
+      test
     }
   })
    export default class App extends Vue {
